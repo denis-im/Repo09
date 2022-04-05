152f749ed46315e636a891653ee5bc9fbb9e374f
zabec <denis.imsirovic@gmail.com>
2022-04-05 15:01:05 +0200

    var animals moved into func

--- ex15/src/index.js
+++ ex15/src/index.js
@@ -1,27 +1,29 @@
 // Only change code below this line
 function switchCase(letter) {
+  var animals;
   switch (letter) {
     case "a":
-      return "antelope";
+      animals = "antelope";
       break;
     case "b":
-      return "bird";
+      animals = "bird";
       break;
     case "c":
-      return "cat";
+      animals = "cat";
       break;
     default:
-      return "stuff";
+      animals = "stuff";
   }
+  return animals;
 }
 
-var animals;
 
-console.log(animals = switchCase("a"));
-console.log(animals = switchCase("b"));
-console.log(animals = switchCase("c"));
-console.log(animals = switchCase("d"));
-console.log(animals = switchCase(4));
+
+console.log(switchCase("a"));
+console.log(switchCase("b"));
+console.log(switchCase("c"));
+console.log(switchCase("d"));
+console.log(switchCase(4));
 
 // Only change code above this line
 module.exports = switchCase;
